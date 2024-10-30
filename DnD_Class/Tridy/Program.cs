be8ee913b8cd1c5bd636d88a8dc33c2470ac2078
ItsMeKubik <102288609+ItsMeKubik@users.noreply.github.com>
2024-10-30 09:07:25 +0100

    Add files via upload

---
+++ DnD_Class/Tridy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tridy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.ID = 1;
+            student.Jmeno = "Petr";
+        }
+    }
+}
